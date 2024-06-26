6f187e8a24254c8d708e8f6a555669391bba2c93
Paul Hagedorn <panzer1119@gmail.com>
2024-06-26 15:54:16 +0200

    chore: add comments to ls_aliases.sh

--- ls_aliases.sh
+++ ls_aliases.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# List with eza
 alias lsa='eza --long --header --group --all --color=always --group-directories-first --time-style long-iso --git --icons=always --classify=always --binary'
+# List with sudo eza
 alias slsa='sudo eza --long --header --group --all --color=always --group-directories-first --time-style long-iso --git --icons=always --classify=always --binary'
+
+# List with watch and eza
 alias wlsa='watch --color eza --long --header --group --all --color=always --group-directories-first --time-style long-iso --git --icons=always --classify=always --binary'
+# List with watch and sudo eza
 alias wslsa='watch --color sudo eza --long --header --group --all --color=always --group-directories-first --time-style long-iso --git --icons=always --classify=always --binary'
